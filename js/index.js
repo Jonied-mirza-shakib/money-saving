8aa09f0f7aab0700e0c58aecbc77a9e1ae96172f
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-02-17 03:33:47 +0600

    ready full website

--- js/index.js
+++ js/index.js
@@ -12,30 +12,60 @@ function updateCalculateButton() {
     let Balance = document.getElementById('balance').innerText = incomeNumber - totalExpenses;
 
 
-    if (incomeNumber < 0) {
+    if (incomeNumber <= 0) {
+        const expensesPrice = document.getElementById('expenses-price').innerText = 00;
+        let Balance = document.getElementById('balance').innerText = 00;
+        const saveAmount = document.getElementById('save-amount').innerText = 00;
+        let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
         document.getElementById('income-input-error').innerText = 'Please positive value';
+        document.getElementById('expenses-error').style.display = 'none';
+    } else if (incomeNumber >= 0) {
+        document.getElementById('income-input-error').style.display = 'none';
+    }
+
+    if (foodNumber <= 0) {
         const expensesPrice = document.getElementById('expenses-price').innerText = 00;
         let Balance = document.getElementById('balance').innerText = 00;
         const saveAmount = document.getElementById('save-amount').innerText = 00;
         let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
-    } else if (foodNumber < 0) {
         document.getElementById('food-input-error').innerText = 'Please positive value';
+        document.getElementById('expenses-error').style.display = 'none';
+    } else if (foodNumber >= 0) {
+        document.getElementById('food-input-error').style.display = 'none';
+    }
+
+    if (rentNumber <= 0) {
         const expensesPrice = document.getElementById('expenses-price').innerText = 00;
         let Balance = document.getElementById('balance').innerText = 00;
         const saveAmount = document.getElementById('save-amount').innerText = 00;
         let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
-    } else if (rentNumber < 0) {
         document.getElementById('rent-input-error').innerText = 'Please positive value';
+        document.getElementById('expenses-error').style.display = 'none';
+    } else if (rentNumber >= 0) {
+        document.getElementById('rent-input-error').style.display = 'none';
+    }
+
+    if (clothesNumber <= 0) {
         const expensesPrice = document.getElementById('expenses-price').innerText = 00;
         let Balance = document.getElementById('balance').innerText = 00;
         const saveAmount = document.getElementById('save-amount').innerText = 00;
         let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
-    } else if (clothesNumber < 0) {
         document.getElementById('clothes-input-error').innerText = 'Please positive value';
+        document.getElementById('expenses-error').style.display = 'none';
+    } else if (clothesNumber >= 0) {
+        document.getElementById('clothes-input-error').style.display = 'none';
+    }
+
+    if (totalExpenses > incomeNumber) {
+        document.getElementById('expenses-error').innerText = 'Your expenses are showing more. You give an account of your exact income.'
         const expensesPrice = document.getElementById('expenses-price').innerText = 00;
         let Balance = document.getElementById('balance').innerText = 00;
         const saveAmount = document.getElementById('save-amount').innerText = 00;
         let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
+        document.getElementById('save-button').disabled = true;
+    } else if (totalExpenses < incomeNumber) {
+        document.getElementById('expenses-error').style.display = 'none';
+        document.getElementById('save-button').disabled = false;
     }
 }
 
@@ -64,7 +94,14 @@ function updateSaveAndTotalBalance() {
         let totalRemainingBalance = parseFloat(remainingBalance.innerText);
         totalRemainingBalance = Balance - discount;
         remainingBalance.innerText = totalRemainingBalance;
+
+        if (incomeNumber === totalExpenses) {
+            const saveAmount = document.getElementById('save-amount').innerText = 00;
+            const remainingBalance = document.getElementById('remaining-balance').innerText = 00;
+        }
     }
+
+
 }
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
@@ -73,7 +110,4 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
 document.getElementById('save-button').addEventListener('click', function () {
     updateSaveAndTotalBalance()
-    // if (saveNumber > 1) {
-
-    // }
 })

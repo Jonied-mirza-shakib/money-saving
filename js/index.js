678844f5ae142bb297957a3202c83562e7d5ebd9
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-02-16 23:33:30 +0600

    style.css complete

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-function updateCalculateButton() {
-    const incomeInput = document.getElementById('income-input');
-    let incomeNumber = parseInt(incomeInput.value);
-    const foodInput = document.getElementById('food-input');
-    let foodNumber = parseInt(foodInput.value);
-    const rentInput = document.getElementById('rent-input');
-    let rentNumber = parseInt(rentInput.value);
-    const clothesInput = document.getElementById('clothes-input');
-    let clothesNumber = parseInt(clothesInput.value);
-    const totalExpenses = foodNumber + rentNumber + clothesNumber;
-    const expensesPrice = document.getElementById('expenses-price').innerText = totalExpenses;
-    let Balance = document.getElementById('balance').innerText = incomeNumber - totalExpenses;
-}
-document.getElementById('calculate-btn').addEventListener('click', function () {
-    updateCalculateButton()
-})
+// function updateCalculateButton() {
+//     const incomeInput = document.getElementById('income-input');
+//     let incomeNumber = parseInt(incomeInput.value);
+//     const foodInput = document.getElementById('food-input');
+//     let foodNumber = parseInt(foodInput.value);
+//     const rentInput = document.getElementById('rent-input');
+//     let rentNumber = parseInt(rentInput.value);
+//     const clothesInput = document.getElementById('clothes-input');
+//     let clothesNumber = parseInt(clothesInput.value);
+//     const totalExpenses = foodNumber + rentNumber + clothesNumber;
+//     const expensesPrice = document.getElementById('expenses-price').innerText = totalExpenses;
+//     let Balance = document.getElementById('balance').innerText = incomeNumber - totalExpenses;
+// }
+// document.getElementById('calculate-btn').addEventListener('click', function () {
+//     updateCalculateButton()
+// })

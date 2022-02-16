cda884d487e2ea1289109a391ab5fbdae22f4e03
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-02-17 01:37:04 +0600

    complete Error handleling

--- js/index.js
+++ js/index.js
@@ -10,10 +10,32 @@ function updateCalculateButton() {
     const totalExpenses = foodNumber + rentNumber + clothesNumber;
     const expensesPrice = document.getElementById('expenses-price').innerText = totalExpenses;
     let Balance = document.getElementById('balance').innerText = incomeNumber - totalExpenses;
+
+
     if (incomeNumber < 0) {
         document.getElementById('income-input-error').innerText = 'Please positive value';
+        const expensesPrice = document.getElementById('expenses-price').innerText = 00;
+        let Balance = document.getElementById('balance').innerText = 00;
+        const saveAmount = document.getElementById('save-amount').innerText = 00;
+        let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
     } else if (foodNumber < 0) {
         document.getElementById('food-input-error').innerText = 'Please positive value';
+        const expensesPrice = document.getElementById('expenses-price').innerText = 00;
+        let Balance = document.getElementById('balance').innerText = 00;
+        const saveAmount = document.getElementById('save-amount').innerText = 00;
+        let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
+    } else if (rentNumber < 0) {
+        document.getElementById('rent-input-error').innerText = 'Please positive value';
+        const expensesPrice = document.getElementById('expenses-price').innerText = 00;
+        let Balance = document.getElementById('balance').innerText = 00;
+        const saveAmount = document.getElementById('save-amount').innerText = 00;
+        let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
+    } else if (clothesNumber < 0) {
+        document.getElementById('clothes-input-error').innerText = 'Please positive value';
+        const expensesPrice = document.getElementById('expenses-price').innerText = 00;
+        let Balance = document.getElementById('balance').innerText = 00;
+        const saveAmount = document.getElementById('save-amount').innerText = 00;
+        let remainingBalance = document.getElementById('remaining-balance').innerText = 00;
     }
 }
 
@@ -42,8 +64,6 @@ function updateSaveAndTotalBalance() {
         let totalRemainingBalance = parseFloat(remainingBalance.innerText);
         totalRemainingBalance = Balance - discount;
         remainingBalance.innerText = totalRemainingBalance;
-    } else if (saveInput.value < 0) {
-        const saveError = document.getElementById('save-error').innerText = 'Please positive value';
     }
 }
 
